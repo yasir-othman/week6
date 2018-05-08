8480c4d4e99f0f2460e4156135275ad600e27c28
yasir-othman <35550050+yasir-othman@users.noreply.github.com>
2018-05-07 22:04:12 -0500

    Create week6.sh

---
+++ week6.sh
@@ -0,0 +1,18 @@
+
+yasir othman
+ 
+ csci2461 networking3- linux
+ 
+ this lab we learned about dmesg command and how to use it
+ to find info regarding system.
+ 
+ sudo dmesg | grep i cpu
+ echo " that command will display the cpu info"
+ 
+ sudo dmesg | grep -i usb
+ echo " the command display info regard the usb"
+ 
+ sudo dmesg -h
+ echo "if you need help use the command above"
+ 
+ 
